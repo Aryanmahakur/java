0b40524b67aa33b0fe2e8121a125f878f35c406f
Aryanmahakur <mahakuraryan@gmail.com>
2025-03-25 12:01:41 +0530

    uf

--- methodss.java
+++ methodss.java
@@ -1,5 +1,23 @@
-class methodss{
-    public static void main(String[] args){
- 
+class methodss {
+     
+    static class Example {  
+        int add(int a, int b) {
+            return a + b;
+        }
+    }
+    static class Examples {  
+        int add(int a, int b) {
+            return a - b;
+        }
+    }
+     
+    public static void main(String[] args) {
+        Example obj1 = new Example();  
+        int sum = obj1.add(5, 10);
+        System.out.println("Sum: " + sum);
+
+        Examples obj2 = new Examples();  // Corrected object creation
+        int difference = obj2.add(5, 10);
+        System.out.println("Difference: " + difference);
     }
 }
